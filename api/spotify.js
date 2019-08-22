29ba8d05ddb0c53602da5a15f8d0f935697ae360
Andy Mina <amina4835@bths.edu>
2019-08-22 11:35:39 -0400

    EZ

--- api/spotify.js
+++ api/spotify.js
@@ -96,6 +96,7 @@ router.post("/connect", (req, res) => {
 		response_type: 'code',
 		client_id: keys.SPOTIFY_CLIENT_ID,
 		scope: scope,
+      state: state,
 		redirect_uri: keys.SPOTIFY_REDIRECT
 	});
 
